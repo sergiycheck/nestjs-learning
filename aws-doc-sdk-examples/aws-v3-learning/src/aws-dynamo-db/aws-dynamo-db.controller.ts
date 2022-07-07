a240cd435dde17d424d369d5bd218fc8b4cd8fd0
serhii.kuzmych <serhii.kuzmych@synergy-way.com>
2022-07-07 13:19:52 +0300

    crud with customers table but hard, use dynamoose ?

--- aws-doc-sdk-examples/aws-v3-learning/src/aws-dynamo-db/aws-dynamo-db.controller.ts
+++ aws-doc-sdk-examples/aws-v3-learning/src/aws-dynamo-db/aws-dynamo-db.controller.ts
@@ -1,23 +1,49 @@
+import { NotEmptyPipe } from './../pipes/not-empty.pipe';
 import { AwsDynamoDbClient } from './aws-dynamo-db.service';
 import { AllExceptionsFromAwsFilter } from './../common/filters/all-exceptions-from-aws.filter';
-import { Controller, Delete, Get, Post, Query, UseFilters } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseFilters,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import {
   CreateTableCommand,
   ListTablesCommand,
   DescribeTableCommand,
   DeleteTableCommand,
+  CreateTableCommandInput,
+  PutItemCommand,
+  PutItemCommandInput,
+  GetItemCommand,
+  UpdateItemCommand,
+  DeleteItemCommand,
 } from '@aws-sdk/client-dynamodb';
-import { DescribeTableDto } from './dto/dto.dto';
+import {
+  DescribeTableDto,
+  CreateTableDto,
+  CreateCustomerDto,
+  GetCustomerDto,
+  UpdateCustomerDto,
+} from './dto/dto.dto';
+
+// core components of dynamodb
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/HowItWorks.CoreComponents.html
 
 @ApiTags('AwsDynamoDbController')
-@Controller('aws-dynamodb-creating-using')
+@Controller('aws-dynamodb')
 @UseFilters(AllExceptionsFromAwsFilter)
 export class AwsDynamoDbController {
   constructor(private cldWS: AwsDynamoDbClient) {}
 
   @Post('create-table')
-  async craateTable() {
+  async craateTable(@Body() createTableDto: CreateTableDto) {
     const params = {
       AttributeDefinitions: [
         {
@@ -43,7 +69,7 @@ export class AwsDynamoDbController {
         ReadCapacityUnits: 1,
         WriteCapacityUnits: 1,
       },
-      TableName: 'TEST_TABLE', //TABLE_NAME
+      ...createTableDto, //TABLE_NAME
       StreamSpecification: {
         StreamEnabled: false,
       },
@@ -65,5 +91,126 @@ export class AwsDynamoDbController {
   @Delete('one-table')
   deleteTable(@Query() dto: DescribeTableDto) {
     return this.cldWS.dDBCl.send(new DeleteTableCommand({ ...dto }));
+  }
+
+  @Post('create-table-customers')
+  async craateTableCustomers(@Body() createTableDto: CreateTableDto) {
+    const params: CreateTableCommandInput = {
+      AttributeDefinitions: [
+        {
+          AttributeName: 'CUSTOMER_ID',
+          AttributeType: 'N',
+        },
+        {
+          AttributeName: 'CUSTOMER_NAME',
+          AttributeType: 'S',
+        },
+      ],
+      KeySchema: [
+        {
+          AttributeName: 'CUSTOMER_ID', //ATTRIBUTE_NAME_1
+          KeyType: 'HASH',
+        },
+        {
+          AttributeName: 'CUSTOMER_NAME', //ATTRIBUTE_NAME_2
+          KeyType: 'RANGE',
+        },
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1,
+      },
+      ...createTableDto, //TABLE_NAME
+      StreamSpecification: {
+        StreamEnabled: true,
+        StreamViewType: 'NEW_AND_OLD_IMAGES',
+      },
+    };
+
+    return await this.cldWS.dDBCl.send(new CreateTableCommand(params));
+  }
+
+  @Post('writing-customer-item/:TableName')
+  createCustomer(
+    @Param('TableName', new NotEmptyPipe('TableName')) TableName: string,
+    @Body() dto: CreateCustomerDto,
+  ) {
+    const params: PutItemCommandInput = {
+      TableName,
+      Item: {
+        CUSTOMER_ID: { N: dto.CUSTOMER_ID },
+        CUSTOMER_NAME: { S: dto.CUSTOMER_NAME },
+      },
+    };
+    return this.cldWS.dDBCl.send(new PutItemCommand(params));
+  }
+
+  @Get('get-customer/:TableName')
+  getCustomer(
+    @Param('TableName', new NotEmptyPipe('TableName')) TableName: string,
+    @Query() dto: GetCustomerDto,
+  ) {
+    const params = {
+      TableName,
+      Key: {
+        CUSTOMER_ID: { N: dto.CUSTOMER_ID },
+        CUSTOMER_NAME: { S: dto.CUSTOMER_NAME },
+      },
+    };
+    return this.cldWS.dDBCl.send(new GetItemCommand(params));
+  }
+
+  @Patch('update-customer/:TableName')
+  updateCustomer(
+    @Param('TableName', new NotEmptyPipe('TableName')) TableName: string,
+    @Body() dto: UpdateCustomerDto,
+  ) {
+    // String: "String",
+    // NumAttribute: 1,
+    // BoolAttribute: true,
+    // ListAttribute: [1, "two", false],
+    // MapAttribute: { foo: "bar" },
+    // NullAttribute: null
+
+    const updateParams = {
+      TableName,
+      Key: {
+        // For example, 'Season': {N:2}.
+        CUSTOMER_ID: { N: dto.CUSTOMER_ID },
+        // For example,  'Episode': {S: "The return"};
+        // (only required if table has sort key).
+        CUSTOMER_NAME: { S: dto.CUSTOMER_NAME },
+      },
+      // Define expressions for the new or updated attributes
+      // For example, "'set Title = :t, Subtitle = :s'"
+      UpdateExpression: 'set email = :t, phone = :s',
+      ExpressionAttributeValues: {
+        // Cannot update attribute CUSTOMER_NAME. This attribute is part of the key
+        // ':id': { N: dto.updateId },
+        // ':n': { S: dto.updateName },
+        // For example ':t' : 'NEW_TITLE'
+        ':t': { S: dto.updateEmail },
+        // For example ':s' : 'NEW_SUBTITLE'
+        ':s': { S: dto.updatePhone },
+      },
+      ReturnValues: 'ALL_NEW',
+    };
+
+    return this.cldWS.dDBCl.send(new UpdateItemCommand(updateParams));
+  }
+
+  @Delete('delete-customer/:TableName')
+  deleteCustomer(
+    @Param('TableName', new NotEmptyPipe('TableName')) TableName: string,
+    @Query() dto: GetCustomerDto,
+  ) {
+    const params = {
+      TableName,
+      Key: {
+        CUSTOMER_ID: { N: dto.CUSTOMER_ID },
+        CUSTOMER_NAME: { S: dto.CUSTOMER_NAME },
+      },
+    };
+    return this.cldWS.dDBCl.send(new DeleteItemCommand(params));
   }
 }
--- aws-doc-sdk-examples/aws-v3-learning/src/aws-dynamo-db/dto/dto.dto.ts
+++ aws-doc-sdk-examples/aws-v3-learning/src/aws-dynamo-db/dto/dto.dto.ts
@@ -1,9 +1,55 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+
+export class CreateTableDto {
+  @IsNotEmpty()
+  @IsString()
+  TableName = 'TEST_TABLE';
+}
 
 export class DescribeTableDto {
   @IsNotEmpty()
   @IsString()
   @ApiProperty({ name: 'TableName', type: String })
   TableName = 'TEST_TABLE';
+}
+
+export class CreateCustomerDto {
+  @IsNotEmpty()
+  @IsNumberString()
+  CUSTOMER_ID = '1';
+
+  @IsNotEmpty()
+  @IsString()
+  CUSTOMER_NAME = 'Richard Roe';
+}
+
+export class GetCustomerDto {
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ name: 'CUSTOMER_ID', type: String })
+  CUSTOMER_ID = '1';
+
+  @IsNotEmpty()
+  @IsString()
+  @ApiProperty({ name: 'CUSTOMER_NAME', type: String })
+  CUSTOMER_NAME = 'Richard Roe';
+}
+
+export class UpdateCustomerDto {
+  @IsNotEmpty()
+  @IsNumberString()
+  CUSTOMER_ID = '3';
+
+  @IsNotEmpty()
+  @IsString()
+  CUSTOMER_NAME = 'Denis Olsem';
+
+  @IsNotEmpty()
+  @IsString()
+  updateEmail = 'someEmail@domain.com';
+
+  @IsNotEmpty()
+  @IsNumberString()
+  updatePhone = '133233233';
 }
